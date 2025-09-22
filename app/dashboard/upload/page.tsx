ba320bb80ef23feaa392edb5f16b4629d7f0113a
kubanovak <novak261@fit.cvut.cz>
2025-09-22 23:33:43 +0200

    add dropzone page

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,12 @@
+import FileUploader from "@/components/ui/FileUploader"
+
+function UploadPage() {
+  return (
+    <div>
+        
+        <FileUploader/>
+    </div>
+  )
+}
+
+export default UploadPage
--- components/ui/PlaceholderDocument.tsx
+++ components/ui/PlaceholderDocument.tsx
@@ -2,10 +2,17 @@
 
 import { PlusCircleIcon } from "lucide-react"
 import { Button } from "./button"
+import { useRouter } from "next/navigation"
 
 function PlaceholderDocument() {
+    const router = useRouter();
+
+    const handleClick = async () => {
+        // Check if user is FREE or over the file limit, go to upgrade page
+        router.push('/dashboard/upload');
+    }
   return (
-    <Button className="flex flex-col items-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400">
+    <Button onClick={handleClick} className="flex flex-col items-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400">
         <PlusCircleIcon className="h-16 w-16"></PlusCircleIcon>
         <p>
             Add a PDF
